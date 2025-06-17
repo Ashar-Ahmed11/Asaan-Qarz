023823ff850a8eb189577ec9f819cb36e9a9eb89
Ashar <ashar.a.hmd11@gmail.com>
2025-06-17 13:35:24 +0500

    initialCommit

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const mongoURI = "mongodb+srv://akhuwat:UIAZGDS@akhuwat-database.l9ey8xt.mongodb.net/asaanqarzdb?retryWrites=true&w=majority&appName=akhuwat-database"
+
+const connectToMongoose = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to Mongoose successfully');
+  } catch (error) {
+    console.error('Error connecting to Mongoose:', error.message);
+  }
+};
+
+module.exports = connectToMongoose;
